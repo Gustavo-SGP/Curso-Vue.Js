b4ab0b62e7d1c1860e791666e003480f9a7b6ce4
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-01-23 21:27:26 -0300

    Código original

---
+++ 07 Filtros/main.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: 'main',
+    data: {
+        juegos: [{
+            titulo: 'Batlefield 1',
+            genero: 'FPS',
+            puntuacion: 9
+        },
+        {
+            titulo: 'Civilization VI',
+            genero: 'Estrategia',
+            puntuacion: 10
+        }
+        {
+            titulo: 'Resident Evil 7',
+            genero: 'Survival Horror',
+            puntuacion: 7
+        }
+        ]
+    },
+})
