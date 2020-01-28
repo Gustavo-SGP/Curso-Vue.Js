06c30b0ed35061a29b37689876510f866830ffe0
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-01-28 16:48:37 -0300

    Nueva propiedad computada

--- 07 Filtros/main.js
+++ 07 Filtros/main.js
@@ -1,6 +1,7 @@
 new Vue({
     el: 'main',
     data: {
+        minimo: 5,
         juegos: [{
             titulo: 'Batlefield 1',
             genero: 'FPS',
@@ -10,7 +11,7 @@ new Vue({
             titulo: 'Civilization VI',
             genero: 'Estrategia',
             puntuacion: 10
-        }
+        },
         {
             titulo: 'Resident Evil 7',
             genero: 'Survival Horror',
@@ -18,4 +19,10 @@ new Vue({
         }
         ]
     },
+
+    computed: {
+        mejoresJuegos() {
+            return this.juegos.filter((juego) => juego.puntuacion >= this.minimo);
+        }
+    }
 })
