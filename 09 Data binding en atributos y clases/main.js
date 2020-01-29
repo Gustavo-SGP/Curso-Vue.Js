628633d0d1bcf6f76ec628bfa7d3fc96ab5117c3
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-01-29 12:04:56 -0300

    Cambio de completao de tarea

--- 09 Data binding en atributos y clases/main.js
+++ 09 Data binding en atributos y clases/main.js
@@ -2,11 +2,17 @@ new Vue({
     el: 'main',
     data: {
         tareas: [
-            {titulo: 'Hacer la compra', completado: false},
-            {titulo: 'Aprender Vue', completado: false},
-            {titulo: 'Aprender Firebase', completado: false},
-            {titulo: 'Dominar ES6', completado: false},
-            {titulo: 'Salir a correr', completado: false},
+            { titulo: 'Hacer la compra', completado: false },
+            { titulo: 'Aprender Vue', completado: false },
+            { titulo: 'Aprender Firebase', completado: false },
+            { titulo: 'Dominar ES6', completado: false },
+            { titulo: 'Salir a correr', completado: false },
         ]
+    },
+
+    methods: {
+        completarTarea(tarea) {
+            tarea.completado = !tarea.completado;
+        }
     }
 });
