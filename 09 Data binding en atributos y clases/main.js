82a72de56b354aa38e221a8f5cc7b47ebdb5dad0
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-01-29 12:29:47 -0300

    Contador tareas completadas

--- 09 Data binding en atributos y clases/main.js
+++ 09 Data binding en atributos y clases/main.js
@@ -7,12 +7,18 @@ new Vue({
             { titulo: 'Aprender Firebase', completado: false },
             { titulo: 'Dominar ES6', completado: false },
             { titulo: 'Salir a correr', completado: false },
-        ]
+        ],
     },
 
     methods: {
         completarTarea(tarea) {
             tarea.completado = !tarea.completado;
+        }
+    },
+
+    computed: {
+        tareasCompletadas() {
+            return this.tareas.filter((tarea) => tarea.completado);
         }
     }
 });
