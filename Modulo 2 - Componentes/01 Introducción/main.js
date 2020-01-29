06ace13edac874becdd35ccc3f8db126a9ad1a98
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-01-29 16:28:29 -0300

    Terminado

--- Modulo 2 - Componentes/01 Introducción/main.js
+++ Modulo 2 - Componentes/01 Introducción/main.js
@@ -1,24 +1,26 @@
 Vue.component('mis-tareas', {
-    props: ['tareas'],
     template: `<ul><li v-for="tarea in tareas">{{ tarea.title }}</li></ul>`,
-});
-
-new Vue({
-    el:'main',
 
     mounted() {
         axios.get('https://jsonplaceholder.typicode.com/todos')
             .then((response) => {
-                this.tareasAjax = response.data;
+                this.tareas = response.data;
             });
     },
 
-    data: {
-        tareasAjax: [],
-        tareasLocales: [
-            {title: 'Hacer la compra'},
-            {title: 'Aprender Vue'},
-            {title: 'Aprender JavaScript ES6'},
-        ]
+    data() {
+        return {
+            tareas: [],
+            // tareasLocales: [
+            //     { title: 'Hacer la compra' },
+            //     { title: 'Aprender Vue' },
+            //     { title: 'Aprender JavaScript ES6' },
+            // ]
+        }
     }
+});
+
+new Vue({
+    el: 'main',
+
 });
