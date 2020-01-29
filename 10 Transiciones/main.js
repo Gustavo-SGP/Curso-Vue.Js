300fea0549beba273e57a89b4662cfcc1185109c
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-01-29 12:43:10 -0300

    Codigo original

---
+++ 10 Transiciones/main.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: 'main',
+    data: {
+        mostrar: true,
+        mensajes: {
+            transicion: 'transiciones CSS con Vue.js',
+            animacion: 'Animaciones CSS con Vue.js',
+            animationCustom: 'Animaciones custom CSS conVue.js',
+            entreElementos: ' Transiciones entre elementos con Vue.js',
+        }
+    },
+});
