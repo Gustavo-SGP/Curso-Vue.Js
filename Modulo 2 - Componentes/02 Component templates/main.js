084eaeff9d630ec6c39d7ed4ad276cd2adb588e1
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-01-31 17:29:31 -0300

    Ganador añadido

--- Modulo 2 - Componentes/02 Component templates/main.js
+++ Modulo 2 - Componentes/02 Component templates/main.js
@@ -1,6 +1,15 @@
 Vue.component('elegir-ganador', {
     porps: ['listado'],
-    template: '<ul><li v-for="persona in listado">{{ persona }}</li></ul>',
+    template: ` <div>
+                    <h1 v-if="ganador">El ganador es: {{ ganador }}</h1>
+                    <template v-else>
+                        <h1>Participantes</h1>
+                        <ul>
+                            <li v-for="persona in listado">{{ persona }}</li>
+                        </ul>
+                        <button @click="elegirGanador">Elegir ganador</button>
+                    </template>
+                </div>`,
     methods: {
         elegirGanador() {
             let cantidad = this.participantes.length;
