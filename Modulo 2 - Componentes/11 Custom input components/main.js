c3823ff1f5a532135f5fd0269f1797d7f10cfc43
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-02-11 03:49:15 -0300

    Contrasena editable

--- Modulo 2 - Componentes/11 Custom input components/main.js
+++ Modulo 2 - Componentes/11 Custom input components/main.js
@@ -1,3 +1,13 @@
+Vue.component('contrasena', {
+    props: ['contrasena'],
+    template: `<input :value="contrasena" @input="comprobarContrasena($event.target.value)">`,
+    methods: {
+        comprobarContrasena(contrasena) {
+            this.$emit('input', contrasena);
+        }
+    }
+});
+
 new Vue({
     el: 'main',
     data: {
