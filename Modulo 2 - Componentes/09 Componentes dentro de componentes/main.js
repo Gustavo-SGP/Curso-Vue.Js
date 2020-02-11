50f513f2aa688fee351bca9c6706d7f4dd8a751f
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-02-11 02:39:00 -0300

    Codigo original

---
+++ Modulo 2 - Componentes/09 Componentes dentro de componentes/main.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: 'main',
+    data: {
+        tareas: [
+            'Finalizar sección Componentes',
+            'Iniciar workflow con Vue CLI y Webpack',
+            'Terminar de estudiar la documentación de Vuex',
+            'Seguir jugando con Vue Router y grabar el primer vídeo',
+        ]
+    }
+});
