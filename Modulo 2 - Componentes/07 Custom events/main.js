ecdadc8384eaa07d3e9a572c0639a75e758cd0f3
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-02-11 00:21:41 -0300

    Mostrar alertas con botones

---
+++ Modulo 2 - Componentes/07 Custom events/main.js
@@ -0,0 +1,26 @@
+Vue.component('alerta', {
+    props: ['tipo', 'posicion'],
+    template: `
+            <section class="alerta" :class="[tipo, posicion]">
+                <header class="alerta__header">
+                    <slot name="header">Hola</slot>
+                </header>
+                <div class="alerta__contenido">
+                    <slot> <!-- solo puede haber un slot sin nombre -->
+                        Lorem ipsum dolor sit amet <!-- texto por defecto -->
+                    </slot>
+                </div>
+                <footer class="alerta__footer">
+                    <slot name="footer">Texto del footer</slot>
+                </footer>
+            </section>`,
+});
+
+new Vue({
+    el: 'main',
+    data: {
+        mostrarExito: false,
+        mostrarError: false,
+        mostrarAdvertencia: false,
+    }
+});
