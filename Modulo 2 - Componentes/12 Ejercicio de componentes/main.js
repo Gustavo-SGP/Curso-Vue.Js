7d4d0101d715a3bb3073c205a2fc183caf76cb41
Gustavo-SGP <59935409+Gustavo-SGP@users.noreply.github.com>
2020-02-11 20:37:08 -0300

    Componente para los usuarios cargando lista de usuarios

--- Modulo 2 - Componentes/12 Ejercicio de componentes/main.js
+++ Modulo 2 - Componentes/12 Ejercicio de componentes/main.js
@@ -1,3 +1,26 @@
+Vue.component('usuarios', {
+    template: '#usuarios-template',  //Template para crear el componente en el html
+    mounted() {     //Capturar cuando esté montado
+        axios.get('https://randomuser.me/api/?results=500')     //Obteniendo 500 usuarios desde la API
+            .then((datos) => {      //Promesa
+                const listado = datos.data.results.map((usuario) => { //Listado de usuarios
+                    return {
+                        nombre: `${usuario.name.title} ${usuario.name.first} ${usuario.name.last}`,
+                        correoe: usuario.email,
+                        foto: usuario.picture.medium, 
+                    }
+                });
+                this.usuarios = listado; //Carga con los usuarios del listado al arreglo de data()
+            });
+    },
+
+    data() {
+        return {
+            usuarios: [],
+        }
+    }
+});
+
 new Vue({
     el: 'main',
 });
